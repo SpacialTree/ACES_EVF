da76070fdbf8e28230048bc43597c1d49d4e104a
Savannah Gramze <savannahgramze@c0706a-s2.ufhpc>
2024-08-13 11:30:12 -0400

    made edits to work with more than one molecule

--- aces_evf/pv_maker.py
+++ aces_evf/pv_maker.py
@@ -15,42 +15,50 @@ from spectral_cube import SpectralCube
 
 save_path = '/orange/adamginsburg/ACES/broadline_sources/EVFs/images/'
 
+# Latitude
+B_MIN = -0.27*u.deg
+B_MAX = 0.22*u.deg
+LIST_B = make_position_list(B_MIN, B_MAX, 0.5*u.arcmin, u.arcmin)
+
+# Longitude
+L_MIN = -0.59*u.deg
+L_MAX = 0.88*u.deg
+LIST_L = make_position_list(L_MIN, L_MAX, 0.5*u.arcmin, u.arcmin)
+
 def make_position_list(amin, amax, step, unit):
     return (np.arange(amin.to(unit).value, amax.to(unit).value+1, step.value)*unit).to(u.deg)
 
-def main():
-    basepath = '/orange/adamginsburg/ACES/mosaics/cubes/'
-    cube_fn = f'{basepath}/CS_CubeMosaic.fits'
-    mol = cube_fn.split('/')[-1].split('_')[0]
-    cube = SpectralCube.read(cube_fn)
-    
-    # Latitude
-    b_min = -0.27*u.deg
-    b_max = 0.22*u.deg
-    list_b = make_position_list(b_min, b_max, 0.5*u.arcmin, u.arcmin)
+def make_pv_b(cube, b, mol):
+    reg = regions.RectangleSkyRegion(center=SkyCoord((L_MIN+L_MAX)/2., b, frame='galactic'), width=1.5*u.deg, height=1*u.arcmin)
+    subcube = cube.subcube_from_regions([reg])
+    pv_mean = subcube.mean(axis=1)
+    pv_mean.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_mean.fits')
+    pv_max = subcube.max(axis=1)
+    pv_max.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_max.fits')
 
-    # Longitude
-    l_min = -0.59*u.deg
-    l_max = 0.88*u.deg
-    list_l = make_position_list(l_min, l_max, 0.5*u.arcmin, u.arcmin)
+def make_pv_l(cube, l, mol):
+    reg = regions.RectangleSkyRegion(center=SkyCoord(l, (B_MIN+B_MAX)/2., frame='galactic'), width=1*u.arcmin, height=0.5*u.deg)
+    subcube = cube.subcube_from_regions([reg])
+    pv_mean = subcube.mean(axis=2)
+    pv_mean.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_mean.fits')
+    pv_max = subcube.max(axis=2)
+    pv_max.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_max.fits')
+
+def make_pv_mol(cube_fn):
+    cube = SpectralCube.read(cube_fn)
+    mol = cube_fn.split('/')[-1].split('_')[0]
 
     for b in list_b:
-        reg = regions.RectangleSkyRegion(center=SkyCoord((l_min+l_max)/2., b, frame='galactic'), width=1.5*u.deg, height=1*u.arcmin)
-        #reg.to_pixel(WCS(head)).plot(ax=ax, edgecolor='red', facecolor='none')
-        subcube = cube.subcube_from_regions([reg])
-        pv_mean = subcube.mean(axis=1)
-        pv_mean.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_mean.fits')
-        pv_max = subcube.max(axis=1)
-        pv_max.save(f'{save_path}/{mol}_pv_b{round(b.value, 3)}_max.fits')
+        make_pv_b(cube, b, mol)
 
     for l in list_l:
-        reg = regions.RectangleSkyRegion(center=SkyCoord(l, (b_min+b_max)/2., frame='galactic'), width=1*u.arcmin, height=0.5*u.deg)
-        #reg.to_pixel(WCS(head)).plot(ax=ax, edgecolor='red', facecolor='none')
-        subcube = cube.subcube_from_regions([reg])
-        pv_mean = subcube.mean(axis=2)
-        pv_mean.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_mean.fits')
-        pv_max = subcube.max(axis=2)
-        pv_max.save(f'{save_path}/{mol}_pv_l{round(l.value, 3)}_max.fits')
+        make_pv_l(cube, l, mol)
+
+def main():
+    basepath = '/orange/adamginsburg/ACES/mosaics/cubes/'
+    cube_fn_CS = f'{basepath}/CS_CubeMosaic.fits'
+    
+    make_pv_mol(cube_fn_CS)
 
 
 if __name__ == "__main__":
