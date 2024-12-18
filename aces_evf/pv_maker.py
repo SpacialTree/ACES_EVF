fd5edc00ea71055ac371e61ffb7f82840482e201
Savannah Gramze <savannahgramze@gmail.com>
2024-12-18 14:02:31 -0500

    switched to dask cube for faster processing

--- aces_evf/pv_maker.py
+++ aces_evf/pv_maker.py
@@ -14,6 +14,7 @@ import spectral_cube
 from spectral_cube import SpectralCube
 import regions
 from regions import Regions
+import time
 
 # Path to where to save the PV diagrams
 save_path = '/orange/adamginsburg/ACES/broadline_sources/EVFs/images/HNCO/'
@@ -64,10 +65,24 @@ def make_pv_b(cube, latitude, mol):
     reg = regions.RectangleSkyRegion(center=SkyCoord((L_MIN + L_MAX) / 2., latitude, frame='galactic'), width=1.5 * u.deg, height=1 * u.arcmin)
     subcube = cube.subcube_from_regions([reg])
     subcube.allow_huge_operations = True
-    pv_mean = subcube.mean(axis=1)
+
+    t = time.process_time()
+    pv_mean = subcube.mean(axis=1, save_to_tmp_dir=True)
+    print(f'Time to calculate mean: {time.process_time() - t}')
+
+    t = time.process_time()
     pv_mean.write(f'{save_path}/{mol}_pv_b{round(latitude.value, 3)}_mean.fits', overwrite=True)
-    pv_max = subcube.max(axis=1)
+    print(f'Time to write mean: {time.process_time() - t}')
+
+    t = time.process_time()
+    pv_max = subcube.max(axis=1, save_to_tmp_dir=True)
+    print(f'Time to calculate max: {time.process_time() - t}')
+
+    t = time.process_time()
     pv_max.write(f'{save_path}/{mol}_pv_b{round(latitude.value, 3)}_max.fits', overwrite=True)
+    print(f'Time to write max: {time.process_time() - t}')
+
+    print('Done with b =', latitude)
 
 
 def make_pv_l(cube, longitude, mol):
@@ -87,10 +102,24 @@ def make_pv_l(cube, longitude, mol):
     reg = regions.RectangleSkyRegion(center=SkyCoord(longitude, (B_MIN + B_MAX) / 2., frame='galactic'), width=1 * u.arcmin, height=0.5 * u.deg)
     subcube = cube.subcube_from_regions([reg])
     subcube.allow_huge_operations = True
+
+    t = time.process_time()
     pv_mean = subcube.mean(axis=2)
+    print(f'Time to calculate mean: {time.process_time() - t}')
+
+    t = time.process_time()
     pv_mean.write(f'{save_path}/{mol}_pv_l{round(longitude.value, 3)}_mean.fits', overwrite=True)
+    print(f'Time to write mean: {time.process_time() - t}')
+
+    t = time.process_time()
     pv_max = subcube.max(axis=2)
+    print(f'Time to calculate max: {time.process_time() - t}')
+
+    t = time.process_time()
     pv_max.write(f'{save_path}/{mol}_pv_l{round(longitude.value, 3)}_max.fits', overwrite=True)
+    print(f'Time to write max: {time.process_time() - t}')
+
+    print('Done with l =', longitude)
 
 def make_pv_mol(cube_fn):
     """
@@ -102,7 +131,8 @@ def make_pv_mol(cube_fn):
         Path to the fits file of the cube.
     """
 
-    cube = SpectralCube.read(cube_fn)
+    cube = SpectralCube.read(cube_fn, use_dask=True)
+    cube.use_dask_scheduler('threads', num_workers=4)
     mol = cube_fn.split('/')[-1].split('_')[0]
 
     list_b = make_position_list(B_MIN, B_MAX)
